091de3a0cd95059cfa65f1212b406e70e3c23e8d
LeeSD90 <lee.doyle90@gmail.com>
2018-05-09 14:41:02 +0200

    Add passenger_booking join table

--- app/models/passenger.rb
+++ app/models/passenger.rb
@@ -1,2 +1,8 @@
 class Passenger < ApplicationRecord
+	has_many	:bookings
+	has_many	:flights
+
+	validates :name,  presence: true
+	validates :email, presence: true,
+	                  format:   { with: /[a-zA-Z]\w*@\w+\.\w{2,4}/ }
 end
